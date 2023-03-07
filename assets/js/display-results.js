47edcb71732e5956d919711e249bd5facc144bd1
Monique <mo.sherred@gmail.com>
2023-03-06 19:29:38 -0800

    apiKeys and other api

---
+++ assets/js/display-results.js
@@ -0,0 +1,4 @@
+const apiKey = 'aB5JMBnZeg0gsdgC7dlA4ZY5ahcjC314ZaSmQfQl'
+
+const apiParks = 'https://developer.nps.gov/api/v1/parks'
+const apiActivities = 'https://developer.nps.gov/api/v1/activities/parks'
